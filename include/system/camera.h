386885a6c6b353a3464f24462d54e1fddeeea49b
Tyler Luu <tluu@ti.com>
2012-02-02 11:14:28 +0200

    Add RAW burst message
    Change-Id: I5b42c66485151d1ba9eea8fbe58d334c2a892c24
    Signed-off-by: Daniel Levin <dendy@ti.com>

--- include/system/camera.h
+++ include/system/camera.h
@@ -90,6 +90,7 @@ enum {
     CAMERA_MSG_FOCUS_MOVE = 0x0800,       // notifyCallback
 #ifdef OMAP_ENHANCEMENT_BURST_CAPTURE
     CAMERA_MSG_COMPRESSED_BURST_IMAGE = 0x1000, // dataCallback
+    CAMERA_MSG_RAW_BURST = 0x2000,        // dataCallback
 #endif
     CAMERA_MSG_ALL_MSGS = 0xFFFF
 };
