TI-OpenLink/libnl

Camera: Adds burst camera message type
Signed-off-by: Emilian Peev <epeev@mm-sol.com>

Changes moved under OMAP_ENHANCEMENT flag.
Signed-off-by: Solaiyappan Saravanan <saravanan.s@ti.com>

Signed-off-by: Vicky Martinez-DeFrain <a0869710@ti.com>
Change-Id: I5ae041ae6062367abd8f8f59879846ae4ef3f72b

include/system/camera.h
@@ -85,6 +85,9 @@ enum {
     // request FRAME and METADATA. Or the apps can request only FRAME or only
     // METADATA.
     CAMERA_MSG_PREVIEW_METADATA = 0x0400, // dataCallback
+#ifdef OMAP_ENHANCEMENT
+    CAMERA_MSG_COMPRESSED_BURST_IMAGE = 0x0800, //dataCallback
+#endif
     CAMERA_MSG_ALL_MSGS = 0xFFFF
 };
 

SciAps/android-system-core

Signed-off-by: Daniel Levin <dendy@ti.com>

include/system/camera.h
@@ -88,6 +88,9 @@ enum {
     // Notify on autofocus start and stop. This is useful in continuous
     // autofocus - FOCUS_MODE_CONTINUOUS_VIDEO and FOCUS_MODE_CONTINUOUS_PICTURE.
     CAMERA_MSG_FOCUS_MOVE = 0x0800,       // notifyCallback
+#ifdef OMAP_ENHANCEMENT_BURST_CAPTURE
+    CAMERA_MSG_COMPRESSED_BURST_IMAGE = 0x1000, // dataCallback
+#endif
     CAMERA_MSG_ALL_MSGS = 0xFFFF
 };
 